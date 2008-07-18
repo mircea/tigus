bea965f2712436622117c959e5200c47d660c228
Adriana Draghici <adriana008@gmail.com>
2008-07-18 19:54:53 +0300

    first commit of Question Editor GUI application
    Signed-off-by: Adriana Draghici <adriana008@gmail.com>

---
+++ app-editor/src/org/tigus/app/editor/QuestionEditor.java
@@ -0,0 +1,18 @@
+package org.tigus.app.editor;
+/**
+ * Main class for the  Question Editor GUI application
+ * 
+ * @author adriana
+ * 
+ */
+public class QuestionEditor {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+       System.out.println("Tigus");
+
+    }
+
+}
