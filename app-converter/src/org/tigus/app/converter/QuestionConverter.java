506c384f1e54d63a6a7d0c67297d54dd02c055bc
Dan Gioga <dgioga@gmail.com>
2008-07-24 15:43:22 +0300

    initial commit for app-converter class
    Signed-off-by: Dan Gioga <dgioga@gmail.com>

---
+++ app-converter/src/org/tigus/app/converter/QuestionConverter.java
@@ -0,0 +1,16 @@
+package org.tigus.app.converter;
+
+public class QuestionConverter {
+
+    /**
+     * application that uses the PlainTextQuestionConverter to create QuestionSet
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println("test");
+
+    }
+
+}
