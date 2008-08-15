cdcd283cae724f0d4d3ef402fa1bf9a4d7db2829
Dan Gioga <dgioga@gmail.com>
2008-08-15 03:28:50 +0300

    conversion: Implemented actual parsing into actual question objects
    Implemented question object parsing
    Implemented a questionPrint function for debug purposes. The function lists only the "well known" tags listed on the tigus concepts page.
    Tested the parser with the challenge_week_10_1.txt file as input.
    Still having trouble with adding questions into questionSets.
    Still have to test with wrong questions and implement error messages.
    Signed-off-by: Dan Gioga <dgioga@gmail.com>

--- conversion/src/org/tigus/conversion/PlainTextQuestionConverter.java
+++ conversion/src/org/tigus/conversion/PlainTextQuestionConverter.java
@@ -5,126 +5,209 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import org.tigus.core.Answer;
+import org.tigus.core.Question;
 
 public class PlainTextQuestionConverter {
-    
+
     /*
      * Class that transforms plain text questions into usable objects
      */
-    
+
     private int state;
-    private final static int NOQUESTIONSTATE = 0;
-    private final static int TAGSTATE = 1;
-    private final static int QUESTIONTEXTSTATE = 2;
-    private final static int ANSWERSTATE = 3;
+    private static final int NOQUESTIONSTATE = 0;
+    private static final int TAGSTATE = 1;
+    private static final int QUESTIONTEXTSTATE = 2;
+    private static final int ANSWERSTATE = 3;
     private File inputFile;
-    //no-question -> tags -> question-text -> answers -> begin
-    
-    public PlainTextQuestionConverter(File input)
-    {
-        inputFile = new File( input.getPath() );
+    // private QuestionSet qSet;
+    private Question q;
+
+    // no-question -> tags -> question-text -> answers -> begin
+
+    public PlainTextQuestionConverter(File input) {
+        inputFile = new File(input.getPath());
+        // qSet = new QuestionSet();
+        q = new Question();
     }
-    
-    public PlainTextQuestionConverter(String path)
-    {
+
+    public PlainTextQuestionConverter(String path) {
         inputFile = new File(path);
+        // qSet = new QuestionSet();
+        q = new Question();
     }
-    
-    private void parseFile()
-    {
+
+    public void parseFile() {
         FileReader inputReader;
-        
+
         try {
             inputReader = new FileReader(inputFile);
             BufferedReader inputBuffer = new BufferedReader(inputReader);
-            
+
             String line;
-            
-            
+
             state = NOQUESTIONSTATE;
-            
-            while((line = inputBuffer.readLine()) != null)
-            {
+
+            //TreeSet<Question> qCollection;
+
+            //qCollection = new TreeSet<Question>();
+            q = new Question();
+
+            while ((line = inputBuffer.readLine()) != null) {
                 /*
                  * if the line is empty and it is not part of the question text
-                 * or we are not in the answer state (where it separates the questions)
-                 * we ignore it  
+                 * or we are not in the answer state (where it separates the
+                 * questions) we ignore it
                  */
-                
-                if( state != QUESTIONTEXTSTATE && state != ANSWERSTATE 
-                        && line.trim().length()==0 )
+
+                if ((state != QUESTIONTEXTSTATE) && (state != ANSWERSTATE)
+                        && (line.trim().length() == 0)) {
                     continue;
-                
+                }
+
                 /*
-                 * if we are in tag state or in no qustion state 
-                 * and the line starts with '@' we have tags to parse
+               
+                 * if we are in tag state or in no qustion state and the line
+                 * starts with '@' we have tags to parse
                  */
-                if( (state == TAGSTATE || state == NOQUESTIONSTATE) 
-                        && line.startsWith("@") )
-                {
+                if (((state == TAGSTATE) || (state == NOQUESTIONSTATE))
+                        && line.startsWith("@")) {
                     state = TAGSTATE;
                     parseTags(line);
                 }
-                
+
                 /*
-                 * if we are in tag state or in question text state 
-                 * and the line doesn't start with '@' nor '+' or'-'
-                 * we have question text to parse
+                 * if we are in tag state or in question text state and the line
+                 * doesn't start with '@' nor '+' or'-' we have question text to
+                 * parse
                  */
-                
-                if( (state == TAGSTATE || state == QUESTIONTEXTSTATE) 
-                        && (!line.startsWith("@"))&& (!line.startsWith("+")) 
-                        && (!line.startsWith("-")) )
-                {
+
+                if (((state == TAGSTATE) || (state == QUESTIONTEXTSTATE))
+                        && (!line.startsWith("@")) && (!line.startsWith("+"))
+                        && (!line.startsWith("-"))) {
                     state = QUESTIONTEXTSTATE;
                     parseQuestionText(line);
                 }
-                
+
                 /*
-                 * if we are in question text state or in answer state 
-                 * and the line starts with '+' or '-' we have answers to parse
+                 * if we are in question text state or in answer state and the
+                 * line starts with '+' or '-' we have answers to parse
                  */
-                
-                if( (state == QUESTIONTEXTSTATE || state == ANSWERSTATE) 
-                        && (line.startsWith("+") || line.startsWith("-")) )
-                {
+
+                if (((state == QUESTIONTEXTSTATE) || (state == ANSWERSTATE))
+                        && (line.startsWith("+") || line.startsWith("-"))) {
                     state = ANSWERSTATE;
                     parseAnswer(line);
                 }
-                
+
                 /*
-                 * if we are in answer state and we have an empty line
-                 * we jump to the next question (no question state)
+                 * if we are in answer state and we have an empty line we jump
+                 * to the next question (no question state)
                  */
-                
-                if( state == ANSWERSTATE && line.trim().length()==0 )
+
+                if ((state == ANSWERSTATE) && (line.trim().length() == 0)) {
                     state = NOQUESTIONSTATE;
-                
-            } 
-            
+                    //qCollection.add(q);
+                    printQuestion();
+                    q = new Question();
+                }
+
+            }
+            // qSet = new QuestionSet(qCollection);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        
+
     }
 
-    private void parseAnswer(String line) 
-    {
-        // TODO Auto-generated method stub    
+    // public QuestionSet getQuestionSet() {
+    // return qSet;
+    // }
+
+    private void printQuestion() {
+        // question printing function for debug purposes only
+
+        System.out.print("\n------------NEW QUESTION-------------\n");
+        System.out.print("----------------TAGS-----------------\n");
+
+        if (q.getTags().containsKey("chapter")) {
+            for (String value : q.getTags().get("chapter")) {
+                System.out.print("chapter-" + value + " ,");
+            }
+        }
+
+        if (q.getTags().containsKey("difficulty")) {
+            for (String value : q.getTags().get("difficulty")) {
+                System.out.print("difficulty-" + value + " ,");
+            }
+        }
+
+        if (q.getTags().containsKey("desc")) {
+            for (String value : q.getTags().get("desc")) {
+                System.out.print("desc-" + value + " ,");
+            }
+        }
+
+        System.out.print("\n----------------TEXT-----------------\n");
+        System.out.print(q.getText());
+
+        System.out.print("\n---------------ANSWER----------------\n");
+        for (Answer ans : q.getAnswers()) {
+            if (ans.isCorrect()) {
+                System.out.print("Correct:" + ans.getText() + "\n");
+            }
+            if (!ans.isCorrect()) {
+                System.out.print("Incorrect:" + ans.getText() + "\n");
+            }
+        }
     }
 
-    private void parseQuestionText(String line) 
-    {
+    private void parseAnswer(String line) {
+
+        // we add the answer according to its true/false value
+        if (line.charAt(0) == '+') {
+            q.addAnswer(true, line.substring(1));
+        }
+        if (line.charAt(0) == '-') {
+            q.addAnswer(false, line.substring(1));
+        }
+    }
+
+    private void parseQuestionText(String line) {
+
+        // if this is the first line we just add it otherwise we concatenate it
+        if (q.getText().isEmpty()) {
+            q.setText(line);
+        } else {
+            q.setText(q.getText().concat("\n"));
+            q.setText(q.getText().concat(line));
+        }
+    }
+
+    private void parseTags(String line) {
         // TODO Auto-generated method stub
-    }
+        String tagName;
 
-    private void parseTags(String line) 
-    {
-        // TODO Auto-generated method stub
+        // we delete the @ character marking the tag line
+        line = line.substring(1);
+
+        // we identify the tag name
+        int i = 0;
+        while (line.charAt(i) != ' ') {
+            i++;
+        }
+
+        // we extract the tag name
+        tagName = new String();
+        tagName = line.substring(0, i);
+
+        // we extract the tag attributes
+        line = line.substring(i + 1);
+
+        // we add the tag name and the tag attributes in the List form
+        q.setTagValueList(tagName, line);
     }
 }
-
-
